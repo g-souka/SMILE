f65274a7d8c747e03d0d6b967aeb11137092dd14
g-souka <19334994+g-souka@users.noreply.github.com>
2017-11-17 18:16:16 +0000

    code comments added
    commented the code with explanations of what each part does and what happens in every moment

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -90,13 +90,16 @@ void ofApp::loadSettings() {
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+
+	// name the program window and set the VSync to true
 	ofSetWindowTitle("SMILE :)");
 	ofSetVerticalSync(true);
 
+	// configure the first state of the captured photos booleans
 	bHasPhotoNeutral = false;
 	bHasPhotoIdeal = false;
 	
-	// print a list of avaiable input devices
+	// print a list of avaiable input devices to the console
     vector<ofVideoDevice> devices = vidGrabber.listDevices();
 
     for(int i = 0; i < devices.size(); i++){
@@ -111,18 +114,18 @@ void ofApp::setup(){
 	camWidth = 640;
 	camHeight = 480;
 
-	// choose device
+	// choose the device you'd like to use, the desired framerate and size to grab
 	vidGrabber.setDeviceID(1);
     vidGrabber.setDesiredFrameRate(60);
     vidGrabber.initGrabber(camWidth, camHeight);
 
 	// allocate memory for photos
-	photoFrameNeutral.allocate(camWidth, camHeight, OF_PIXELS_RGB);
-	photoFrameNeutralTexture.allocate(photoFrameNeutral);
-	photoFrameIdeal.allocate(camWidth, camHeight, OF_PIXELS_RGB);
-	photoFrameIdealTexture.allocate(photoFrameIdeal);
+	photoFrameNeutral.allocate(camWidth, camHeight, OF_PIXELS_RGB);	// relative to the ofPixels variable
+	photoFrameNeutralTexture.allocate(photoFrameNeutral);			// relative to the ofTexture variable
+	photoFrameIdeal.allocate(camWidth, camHeight, OF_PIXELS_RGB);	// the dimensions and pixel type must be specified
+	photoFrameIdealTexture.allocate(photoFrameIdeal);				// the textures are relative to the pixels variables
 
-	// face tracker variable configuration
+	// face tracker variable configuration, the mouth values are extracted from these
 	mouthWidth = tracker.getGesture(ofxFaceTracker::MOUTH_WIDTH);
 	mouthHeight = tracker.getGesture(ofxFaceTracker::MOUTH_HEIGHT);
 
@@ -130,10 +133,10 @@ void ofApp::setup(){
 	loadSettings();
 
 	// load images
-	text01.load("images/text01.png");
-	text02.load("images/text02.png");
-	text03.load("images/text03.png");
-	text04.load("images/text04.png");
+	text01.load("images/text01.png");		// all of this images are external to the program
+	text02.load("images/text02.png");		// they are stored in a root/data/images folder structure
+	text03.load("images/text03.png");		// the root folder must be where the executable
+	text04.load("images/text04.png");		// is also present so that the images are properly fetched
 	text05.load("images/text05.png");
 	text06.load("images/text06.png");
 	text07.load("images/text07.png");
@@ -156,16 +159,16 @@ void ofApp::setup(){
 	bTimerIdealReached = false;
 	bTimerEndReached = false;
 
-	endTimeNeutralPhoto = 2000;
-	endTimeIdealPhoto = 2000;
-	endTimeFinalReset = 10000;
+	endTimeNeutralPhoto = 2000;		// these variables define, in miliseconds,
+	endTimeIdealPhoto = 2000;		// how much time the program is supposed to wait
+	endTimeFinalReset = 10000;		// until it triggers the programmed actions
 
 	startTime = ofGetElapsedTimeMillis();
 
 	// mouth value scaling
-	pctWidth = ofMap(mouthWidth, 0.0, 20.0, 0.0, 1.0, false);
-	pctHeight = ofMap(mouthHeight, 0.0, 8.0, 0.0, 1.0, false);
-	pctMouth = pctWidth * 0.7 + pctHeight * 0.3;
+	pctWidth = ofMap(mouthWidth, 0.0, 20.0, 0.0, 1.0, false);	// the values of Mouth are scaled using a map function
+	pctHeight = ofMap(mouthHeight, 0.0, 8.0, 0.0, 1.0, false);	// the mapping is done to 0. to 1. range
+	pctMouth = pctWidth * 0.7 + pctHeight * 0.3;				// this allows for an easier manipulation of the representation of these values
 
 	// text bool configuration
 	bShowText = false;
@@ -179,7 +182,7 @@ void ofApp::setup(){
 void ofApp::update(){
 
 	// in order to update in real time
-	// the following variables must also be set in update()
+	// some of the following variables must also be set in update()
 
 	// update mouth values
 	mouthWidth = tracker.getGesture(ofxFaceTracker::MOUTH_WIDTH);
@@ -215,11 +218,13 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-	ofBackground(ofColor::white);
-	ofSetColor(ofColor::white);
+	ofBackground(ofColor::white);	// configure the initial layout of the application
+	ofSetColor(ofColor::white);		// here the background and text colors are defined
+	ofSetLineWidth(5);				// set the line width to 5
 
-	ofSetLineWidth(5);
-
+	// some moments feature black backgrounds instead of white
+	// as such, it is mandatory to change what LOGO image is displayed in the top right corner
+	// since moment 6 and 7 have black backgrounds, the image that is draw must be white to be seen
 	if (momentNum == 6 || momentNum == 7) {
 		logoSmileInverted.draw(ofGetWindowWidth() - logoSmileInverted.getWidth() - 10, 10);
 	}
@@ -228,11 +233,19 @@ void ofApp::draw(){
 	}
 
 	// moment 01
+	// in this first moment the first text element is drawn
+	// it is centered in the program window, this is done in a responsive way
+	// if the window is scaled, the text element stays centered
 	if (momentNum == 1) {
 		text01.draw(windowCenterW - text01.getWidth() * 0.5, windowCenterH - text01.getHeight() * 0.5);
 	}
 
 	// moment 02
+	// text element draw at center
+	// timer is reset and starts as soon as moment 02 is entered
+	// when the timer reaches the predetermined time a photo is secretly taken to the user
+	// the photo is stored in memory and exported to an external file named "TimeStamp - photoFrameNeutral.jpg"
+	// the bool for the photo is changed to true and the bool for the timer changes state
 	if (momentNum == 2) {
 		text02.draw(windowCenterW - text02.getWidth() * 0.5, windowCenterH - text02.getHeight() * 0.5);
 
@@ -256,9 +269,11 @@ void ofApp::draw(){
 		}
 
 		if (bHasPhotoNeutral == false && bTimerNeutralReached) {
+			// output a message to the console if the conditions are proven
 			ofMessage msg("  Neutral Timer Reached\n  photoFrameNeutralTexture loaded\n  !bTimerNeutralReached");
 			ofSendMessage(msg);
 
+			// save the image to an external file
 			ofPixels & pixels = vidGrabber.getPixels();
 			for (int i = 0; i < pixels.size(); i++) {
 				photoFrameNeutral[i] = pixels[i];
@@ -266,24 +281,30 @@ void ofApp::draw(){
 			photoFrameNeutralTexture.loadData(photoFrameNeutral);
 			ofSaveImage(photoFrameNeutral, ofToString(ofGetTimestampString() + " - photoFrameNeutral") + ".jpg");
 
+			// timer and photo bool
 			bHasPhotoNeutral = true;
 			bTimerNeutralReached = !bTimerNeutralReached;
 		}
 	}
 
 	// moment 03
+	// display the text element at the center of the window
 	if (momentNum == 3) { text03.draw(windowCenterW - text03.getWidth() * 0.5, windowCenterH - text03.getHeight() * 0.5); }
 
 	// moment 04
+	// display the text element at the center of the window
 	if (momentNum == 4) { text04.draw(windowCenterW - text04.getWidth() * 0.5, windowCenterH - text04.getHeight() * 0.5); }
 
 	// moment 05
+	// display the text element at the center of the window
 	if (momentNum == 5) { text05.draw(windowCenterW - text05.getWidth() * 0.5, windowCenterH - text04.getHeight() * 0.5); }
 
 	// moment 06
+	// this moment differentiates itself by having a black background, the logo is white also
+	// text elements are no longer present and now the user must interact with the app through his smile
+	// the camera input is shown at the center and below it there is a meter that represents the smile amount
 	if (momentNum == 6) {
 		ofBackground(ofColor::black);
-        //ofBackground(127 + 255 * (pctMouth * 0.5));
 		logoSmileInverted.draw(ofGetWindowWidth() - logoSmileInverted.getWidth() - 10, 10);
 
 		// show the camera input
@@ -296,9 +317,10 @@ void ofApp::draw(){
         ofDrawRectangle(windowCenterW - (camWidth * 0.5), windowCenterH - (camHeight * 0.5), camWidth, camHeight);
     }
 
-	// found face
+	// if the face is found
 	if (momentNum == 6 && tracker.getFound()) {
         
+		// if the text is set to show, the screen will have information relating to different variables used for debugging
 		if (bShowText) {
 			ofDrawBitmapString("Start Time: " + ofToString(startTime, 1), 20, ofGetHeight() - 220);
 			ofDrawBitmapString("End Time:   " + ofToString(endTimeIdealPhoto / 1000.0, 1) + " seconds", 20, ofGetHeight() - 200);
@@ -309,7 +331,7 @@ void ofApp::draw(){
 			ofDrawBitmapString("Mouth Height: " + ofToString(mouthHeight), 20, ofGetHeight() - 100);
 		}
 
-		// horizontal bar showing smile progress
+		// horizontal meter bar showing smile progress
 		ofFill();
 		ofSetColor(20, 20, 20);
 		ofDrawRectangle(windowCenterW - (camWidth * 0.5), windowCenterH + (camHeight * 0.5) + 20, camWidth, 20);
@@ -325,6 +347,7 @@ void ofApp::draw(){
 		ofDrawLine(windowCenterW + (camWidth * 0.25), windowCenterH + (camHeight * 0.5) + 20, windowCenterW + (camWidth * 0.25), windowCenterH + (camHeight * 0.5) + 40);
 		
 		// SMILE text
+		// the text below the bar must be correctly placed in order to understand where the threshold is
 		ofSetColor(ofColor::white);
 		textSmile.draw(windowCenterW + (camWidth * 0.25), windowCenterH + (camHeight * 0.5) + 50);
 
@@ -333,7 +356,8 @@ void ofApp::draw(){
 			ofSetColor(255, 255, 255, 55 + 220 * alphaOscillation);
 			ofSetLineWidth(1);
 
-			// overlap the mesh with the camera display
+			// overlap the mesh with the camera display by translation
+			// this must be done in order to have the mesh on top of the "real" face
 			ofPushView();
 			ofTranslate(ofGetWindowWidth() * 0.5 - (vidGrabber.getWidth() * 0.5), ofGetWindowHeight() * 0.5 - (vidGrabber.getHeight() * 0.5));
 			tracker.getImageMesh().drawWireframe();
@@ -344,16 +368,16 @@ void ofApp::draw(){
 		ofSetColor(ofColor::white);
 
 		if (bHasPhotoIdeal == false) {
-			if(pctMouth < 0.75) {	// this value indicates how much the people has to smile, range is [0, 1]
-				startTime = ofGetElapsedTimeMillis();
+			if(pctMouth < 0.75) {	// this value is the threshold and indicates how much the people has to smile, range is [0, 1]
+				startTime = ofGetElapsedTimeMillis();	// if the smile is below the threshold level, the timer keeps resetting (stays at zero)
 			}
-			else {
+			else { // the moment the threshold is passed, start to count
 				float timer = ofGetElapsedTimeMillis() - startTime;
 				if (timer >= endTimeIdealPhoto && !bTimerIdealReached) {
-					bTimerIdealReached = true;
+					bTimerIdealReached = true; // if the condition is held for sufficient time, set the timer bool to true
 				}
 
-				// get the percentage of the timer
+				// get the percentage of the timer, used for debug only
 				float pct = ofMap(timer, 0.0, endTimeIdealPhoto, 0.0, 1.0, true);
 
 				if (bShowText) {
@@ -366,10 +390,12 @@ void ofApp::draw(){
 					ofDrawBitmapString(info, 20, 20);
 				}
 
-				if (bTimerIdealReached) {
+				if (bTimerIdealReached) {	// when the timer is reached, take the Idealized photo and proceed to the next moment
+					// output to the console that the timer, photo and the bool have been taken/changed
 					ofMessage msg("  Ideal Timer Reached\n  photoFrameIdealTexture loaded\n  !bTimerIdealReached");
 					ofSendMessage(msg);
 
+					// generate the photo and save it to an external file
 					ofPixels & pixels = vidGrabber.getPixels();
 					for (int i = 0; i < pixels.size(); i++) {
 						photoFrameIdeal[i] = pixels[i];
@@ -377,7 +403,9 @@ void ofApp::draw(){
 					photoFrameIdealTexture.loadData(photoFrameIdeal);
 					ofSaveImage(photoFrameIdeal, ofToString(ofGetTimestampString() + " - photoFrameIdeal") + ".jpg");
 
+					// set the photo boolean to true
 					bHasPhotoIdeal = true;
+					// change the timer bool state
 					bTimerIdealReached = !bTimerIdealReached;
 
 					// jump automatically to moment 07 when the picture is taken
@@ -387,11 +415,16 @@ void ofApp::draw(){
 		}
 	}
 
+	// if no face is found, output text that a face is being searched for, used for debugging purposes
 	if (momentNum == 6 && tracker.getFound() == false && bShowText) {
 		ofDrawBitmapString("Searching for face...", 20, ofGetHeight() - 140);
 	}
 
 	// moment 07
+	// show the Neutral photo at the center of the screen
+	// below the photo place the text that asks the user what is happiness
+	// contrary to the expectations the photo shown here is the neutral
+	// and not the ideal one that was taken the moment before
 	if (momentNum == 7) {
 		ofBackground(ofColor::black);
 		ofSetColor(ofColor::white);
@@ -413,6 +446,9 @@ void ofApp::draw(){
 	}
 
 	// moment 08
+	// display both the photos side-by-side in order to compare them
+	// label the left one as the neutral/natural
+	// the right one as the idealized/projected
 	if (momentNum == 8) {
 		ofBackground(ofColor::black);
 		ofSetColor(ofColor::white);
@@ -438,6 +474,8 @@ void ofApp::draw(){
 	}
 
 	// moment 09
+	// keep showing the two photos
+	// change the text to a message about both versions of the subject being a part of who you are
 	if (momentNum == 9) {
 		ofBackground(ofColor::black);
 		ofSetColor(ofColor::white);
@@ -461,24 +499,31 @@ void ofApp::draw(){
 	}
 
 	// moment 10
+	// display the text element at the center of the window
 	if (momentNum == 10) {
 		text06.draw(windowCenterW - text06.getWidth() * 0.5, windowCenterH - text06.getHeight() * 0.5);
 	}
 	
 	// moment 11
+	// display the text element at the center of the window
 	if (momentNum == 11) {
 		text07.draw(windowCenterW - text07.getWidth() * 0.5, windowCenterH - text07.getHeight() * 0.5);
 	}
 
 	// moment 12
+	// display the text element at the center of the window
+	// start timer when moment is entered that automatically resets the app
+	// this reset allows the app to be autonomous and restart from the beggining
+	// for every new user that interacts with the installation
 	if (momentNum == 12) {
 		text08.draw(windowCenterW - text08.getWidth() * 0.5, windowCenterH - text08.getHeight() * 0.5);
 
+		// initiate timer
 		float timer = ofGetElapsedTimeMillis() - startTime;
 		if (timer >= endTimeFinalReset && bTimerEndReached == false) {
 			bTimerEndReached = true;
 
-			// get the percentage of the timer
+			// get the percentage of the timer, used for debbuging
 			float pct = ofMap(timer, 0.0, endTimeFinalReset, 0.0, 1.0, true);
 
 			if (bShowText) {
@@ -491,17 +536,21 @@ void ofApp::draw(){
 				ofDrawBitmapString(info, 20, 20);
 			}
 
+			// output to the console messages about the state changes and timer
 			ofMessage msg("  End Timer Reached\n  bHasPhotoNeutral = false\n  bHasPhotoIdeal = false");
 			ofSendMessage(msg);
 
+			// set the photo bools to false so that the next user can take new pictures
 			bHasPhotoNeutral = false;
 			bHasPhotoIdeal = false;
+			// set the moment to the first one, so that it starts ate the beggining
 			momentNum = 1;
+			// change the state of the timer bool so that it works every time a new user is interacting
 			bTimerEndReached = !bTimerEndReached;
 		}
 	}
 
-	// camera is paused
+	// camera is paused, used for debugging
 	if (bPaused && bShowText) {
 		ofSetColor(0);
 		ofDrawBitmapString("Paused", 20, ofGetHeight() - 160);
@@ -512,6 +561,7 @@ void ofApp::draw(){
 		gui.draw();
 	}
 
+	// show the text
 	if (bShowText) {
 		ofSetColor(ofColor::hotPink);
 		ofDrawBitmapString("alphaOscillation " + ofToString(alphaOscillation), 20, ofGetHeight() - 80);
@@ -523,6 +573,10 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::setVideoSource(bool useCamera) {
+
+	// this void element was imported from the ofxFaceTracking addon
+	// it checks if a camera input is to be used
+	// and attributes several values to the variables used by the tracker
 
 	bUseCamera = useCamera;
 
@@ -536,33 +590,33 @@ void ofApp::setVideoSource(bool useCamera) {
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
-	if(key == 's' || key == 'S'){
-        vidGrabber.videoSettings();
-    }
+	if(key == 's' || key == 'S'){	// opens a separate window that allows the camera to be controlled
+        vidGrabber.videoSettings();	// this allows to manipulate some aspects like brightness, gain, white balance, etc.
+    }								// every camera allows different types of control
 
 	switch (key) {
 	
-		case 't':
+		case 't':	// toggle the display of the text
 			bShowText = !bShowText;
 			break;
 
-		case 'r':
+		case 'r':	// resets the face tracker 
 			tracker.reset();
 			break;
 
-		case 'm':
+		case 'm':	// toggles the face mesh drawing
 			bDrawMesh = !bDrawMesh;
 			break;
 
-		case 'p':
+		case 'p':	// pauses/unpauses the video input
 			bPaused = !bPaused;
 			break;
 
-		case 'g':
+		case 'g':	// toggles the GUI
 			bGuiVisible = !bGuiVisible;
 			break;
             
-        case 'b':
+        case 'b':	// toggles the capability to move back in the moments with the RMB
             bAllowBackwards = !bAllowBackwards;
             break;
 	}
@@ -585,14 +639,16 @@ void ofApp::mouseDragged(int x, int y, int button){
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 
-    if(bAllowBackwards) {
-        if (button == 2 && momentNum > 1) momentNum--; // if backward movement is not allowed the RMB does nothing
+    if(bAllowBackwards) { // if backward movement is not allowed the RMB does nothing
+        if (button == 2 && momentNum > 1) momentNum--;
         else if (button == 0 && momentNum < 12)	momentNum++;
     }
-    else {
-        if (button == 0 && momentNum < 12 && momentNum != 6) momentNum++; // in moment 6, the only way to move is by using your smile, LMB does not work
+    else { // in moment 6, the only way to move is by using your smile, LMB does not work
+        if (button == 0 && momentNum < 12 && momentNum != 6) momentNum++;
     }
 
+	// in the following moment variables the timers are reset
+	// so that the counting is made from 0 to the defined time
 	if (momentNum == 2) {
 		startTime = ofGetElapsedTimeMillis();
 	}
@@ -626,6 +682,7 @@ void ofApp::windowResized(int w, int h){
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
+	// receive the messages and output them to the console
 	cout << "Got the message: \n" + msg.message << endl;
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,7 +9,8 @@ class ofApp : public ofBaseApp, public FaceOsc {
 
     public:
 
-		void loadSettings();
+		void loadSettings();	// this void is used to load the settings present in the settings.xml file
+								// this was imported from the face tracking examples of the addon
 
         void setup();
         void update();
@@ -29,49 +30,57 @@ class ofApp : public ofBaseApp, public FaceOsc {
   
     
 		// webcam input and video frame variables
-		ofVideoGrabber vidGrabber;
-		ofPixels photoFrameNeutral;
-		ofTexture photoFrameNeutralTexture;
-		ofPixels photoFrameIdeal;
-		ofTexture photoFrameIdealTexture;
+		ofVideoGrabber vidGrabber;			// the video grabber is used to grab the camera input
+		ofPixels photoFrameNeutral;			// to store the information for the Neutral and Ideal picture,
+		ofTexture photoFrameNeutralTexture;	// obtained from the camera, ofPixels is used - this information is stored in the cpu
+		ofPixels photoFrameIdeal;			// in order to draw the ofPixels, ofTexture is used, this information is stored in the gpu
+		ofTexture photoFrameIdealTexture;	// it is from these ofPixels variables that the external images are generated
 
 		// video, face and window dimension variables
-        int camWidth, camHeight;
-		int windowCenterW, windowCenterH;
-		int sourceWidth, sourceHeight;
-		float mouthHeight, mouthWidth;
+        int camWidth, camHeight;			// in order to simplify the placement of elements
+		int windowCenterW, windowCenterH;	// these variables were used to have a responsive behaviour
+		int sourceWidth, sourceHeight;		// to the window size and input sizes
+		float mouthWidth, mouthHeight;
 
+		// face tracking variables
 		ofBaseVideoDraws *videoSource;
 		ofxFaceTracker tracker;
 		ofMatrix4x4 rotationMatrix;
+
+		// gui for OSC communication variable
 		ofxPanel gui;
 
+		// video source void (also imported from the addon examples)
 		void setVideoSource(bool useCamera);
 
 		// boolean variables
-		bool bUseCamera;
-		bool bPaused;
-		bool bDrawMesh;
-		bool bGuiVisible;
-		bool bHasPhotoNeutral;
-		bool bHasPhotoIdeal;
-		bool bShowText;
-        bool bAllowBackwards; // to prevent the user to go back in the slides
+		bool bUseCamera;		// use a camera
+		bool bPaused;			// pause the camera input
+		bool bDrawMesh;			// decide if the face mesh is drawn
+		bool bGuiVisible;		// toggle the GUI visibility
+		bool bHasPhotoNeutral;	// bool that determines if the Neutral photo has been taken
+		bool bHasPhotoIdeal;	// bool that determines if the Ideal photo has been taken
+		bool bShowText;			// used to toggle the text, used for debugging
+        bool bAllowBackwards;	// used to prevent the user to go back in the slides
 
 		// image variables
-		ofImage text01, text02, text03, text04, text05, text06, text07, text08;
-		ofImage logoSmile, logoSmileInverted;
+		ofImage text01, text02, text03, text04, text05, text06, text07, text08;		// these ofImage variables hold the different external images that are used
+		ofImage logoSmile, logoSmileInverted;										// all the text and logos are stored in these variables and then shown when called
 		ofImage textSmile, textNatural, textProjection, textBoth, textHappiness;
-		int momentNum;
+
+		// moment variables
+		int momentNum;			// this variable is used to determine which moment is the app currently in
+								// each moment has it's own visual characteristics and this is used to change what is shown
 		
 		// timer variables
 		float startTime;
-		float endTimeNeutralPhoto, endTimeIdealPhoto, endTimeFinalReset;
-		bool bTimerNeutralReached, bTimerIdealReached, bTimerEndReached;
+		float endTimeNeutralPhoto, endTimeIdealPhoto, endTimeFinalReset;	// each momment where a timer is used had a timer specific to that situation
+		bool bTimerNeutralReached, bTimerIdealReached, bTimerEndReached;	// the time would be defined in the endTime variables
+																			// the bools were used to determine that the timers had been reached
 
 		// alpha oscillation variables
-		float t;
-		float alphaOscillation;
+		float t;				// these were used to add a transparency oscillation to the face mesh
+		float alphaOscillation;	// that is drawn on top of the users face
 
 		// mouth percentage values variables
 		float pctWidth, pctHeight, pctMouth; // pct means percentage
